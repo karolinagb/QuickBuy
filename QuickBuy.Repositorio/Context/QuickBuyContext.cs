907cbdfb1e7af2b81c7c5af71247b93f669ae1f0
Karolina Gomes <63608020+karolinagb@users.noreply.github.com>
2020-07-27 18:09:32 -0300

    Criada a classe Context com os DbSet's e adicionado pacote do Entity Framework para MySql e pacote Proxies

---
+++ QuickBuy.Repositorio/Context/QuickBuyContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickBuy.Dominio.Entities;
+using QuickBuy.Dominio.Entities.ValuableObject;
+
+namespace QuickBuy.Repositorio.Context
+{
+    class QuickBuyContext : DbContext
+    {
+        public DbSet<User> User { get; set; }
+
+        public DbSet<Product> Product { get; set; }
+
+        public DbSet<Request> Request { get; set; }
+
+        public DbSet<OrderItem> OrderItem { get; set; }
+
+        /*O PaymentForm é um objeto de valor, mas precisamos dele na base de dados:*/
+        public DbSet<PaymentForm> PaymentForm { get; set; }
+    }
+}
