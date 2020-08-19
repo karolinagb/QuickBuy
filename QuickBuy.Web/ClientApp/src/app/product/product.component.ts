ee65c4a611bb38863948d767c3a2fe6614527c42
Karolina Gomes <63608020+karolinagb@users.noreply.github.com>
2020-08-19 15:23:25 -0300

    Configurando a classe ProductComponent para funcionar como Componente do Angular

--- QuickBuy.Web/ClientApp/src/app/product/product.component.ts
+++ QuickBuy.Web/ClientApp/src/app/product/product.component.ts
@@ -1,3 +1,11 @@
+import { Component } from "@angular/core"
+
+//Utilizando um Decorator que vai configurar o ProductComponent
+@Component({
+  selector: "",
+  template: "<html><body>{{ getName() }}</body></html>"
+})
+
 //export é como o public no c#
 export class ProductComponent {
   public name: string;
@@ -7,6 +15,6 @@ export class ProductComponent {
   //Nome de variáveis internas e métodos em TypeScript sempre começa com letra minúscula(camelCase)
 
   public getName(): string {
-    return this.name;
+    return "Samsung";
   }
 }
