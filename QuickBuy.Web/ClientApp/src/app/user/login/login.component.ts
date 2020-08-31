9841c3ee83de1831998c7b54ce9f297da200a5d8
Karolina Gomes <63608020+karolinagb@users.noreply.github.com>
2020-08-31 15:43:58 -0300

    Criado a classe TypeScript para o Component Login

---
+++ QuickBuy.Web/ClientApp/src/app/user/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+//Para o typescript entender a classe como component
+@Component({
+  //Definindo uma tag que pode ser usada para renderizar o LoginComponent
+  selector: "app-login",
+  //Arquivo html que representa o template do componente:
+  templateUrl: "./login.component.html",
+  //informando o arquivo de folha de estilo do componente:
+  //OBS: Dentro de colchetes pois é uma lista. Posso colocar várias folhas de estilo
+  styleUrls: ["./login.component.css"]
+})
+export class LoginComponent {
+
+}
